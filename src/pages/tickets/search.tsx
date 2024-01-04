56c195748c709f739115ef54062077648d9c79f9
Trần Đại <tdai2903@gmail.com>
2024-01-04 16:16:28 +0700

    [Git] Refactor: Update dev branch

--- src/pages/tickets/search.tsx
+++ src/pages/tickets/search.tsx
@@ -15,6 +15,8 @@ const SearchPage: React.FunctionComponent = () => {
   const profile = JSON.parse(localStorage.getItem("profile") || "{}"); // lấy profile từ local storage
   const { configView, fetchTickets } = useService();
 
+  const test = 21;
+
   //Format date time
   function formatCreatedTime(createdTime) {
     const date = new Date(createdTime);
